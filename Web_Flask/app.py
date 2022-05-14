ef944a6347eb6cee9083f625899cbc8b625a04a4
Ryan Dobbe <rydobbe@gmail.com>
2022-05-14 10:49:22 -0500

    separated models into models.py added jinja snippet template inheritance

--- Web_Flask/app.py
+++ Web_Flask/app.py
@@ -1,59 +1,20 @@
 #Imports
-from datetime import date
-from enum import unique
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import db, Client
 
 
 #Configure Flask
 app = Flask(__name__)
 app.secret_key = "hehehe"
 
+
 # DB Connection/Config
 db_name = "sqlite_database.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-# DB will be used for all SQLAlchemy commands
-db = SQLAlchemy(app)
-
-# # DB Model
-class Client(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    firstName = db.Column(db.String(255), nullable=False)
-    lastName = db.Column(db.String(255), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    city = db.Column(db.String(50), nullable=False)
-    state = db.Column(db.String(10), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    phone = db.Column(db.Integer(), nullable=False)
-    socialSecurity = db.Column(db.Integer(), nullable=False)
-    dateOfBirth = db.Column(db.Integer(), nullable=False)
-    annualIncome = db.Column(db.Integer(), nullable=False)
-    monthlyRent = db.Column(db.Integer(), nullable=False)
-    zipCode = db.Column(db.Integer(), nullable=False)
-    country = db.Column(db.String(50), nullable=False)
-    
-
-    
-    def __init__(self, firstName, lastName, address, city,
-                        state, email, phone, socialSecurity,
-                        dateOfBirth, annualIncome, monthlyRent,
-                        zipCode, country):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.address = address
-        self.city = city
-        self.state = state
-        self.email = email
-        self.phone = phone
-        self.socialSecurity = socialSecurity
-        self.dateOfBirth = dateOfBirth
-        self.annualIncome = annualIncome
-        self.monthlyRent = monthlyRent
-        self.zipCode = zipCode
-        self.country = country
-
+db.app = app
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -94,6 +55,7 @@ def registered_clients():
 def view_clients():
     return redirect(url_for("registered_clients"))
 
+
 @app.route("/drop", methods=['POST'])
 def drop():
     db.drop_all()
@@ -101,13 +63,16 @@ def drop():
     db.session.commit()
     return redirect(url_for("index"))
 
+
 @app.route("/add", methods=['POST'])
 def add():
     return redirect(url_for("index"))
 
+
 @app.route("/client_view")
 def client_view():
     return render_template("client_view.html")
+
 
 # __name__ is set to __main__ at runtime when running app directly
 if __name__ == '__main__':
---
+++ Web_Flask/models.py
@@ -0,0 +1,40 @@
+# Imports
+from flask_sqlalchemy import SQLAlchemy
+
+db= SQLAlchemy()
+
+class Client(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    firstName = db.Column(db.String(255), nullable=False)
+    lastName = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.Integer(), nullable=False)
+    socialSecurity = db.Column(db.Integer(), nullable=False)
+    dateOfBirth = db.Column(db.Integer(), nullable=False)
+    annualIncome = db.Column(db.Integer(), nullable=False)
+    monthlyRent = db.Column(db.Integer(), nullable=False)
+    zipCode = db.Column(db.Integer(), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, firstName, lastName, address, city,
+                        state, email, phone, socialSecurity,
+                        dateOfBirth, annualIncome, monthlyRent,
+                        zipCode, country):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.address = address
+        self.city = city
+        self.state = state
+        self.email = email
+        self.phone = phone
+        self.socialSecurity = socialSecurity
+        self.dateOfBirth = dateOfBirth
+        self.annualIncome = annualIncome
+        self.monthlyRent = monthlyRent
+        self.zipCode = zipCode
+        self.country = country
+        
+        
